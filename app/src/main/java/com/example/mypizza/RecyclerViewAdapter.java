3e90f4be9392e43bc294e3aa4b6ddc2ad1e70bdf
admin <kkanat94@gmail.com>
2021-02-22 00:22:27 +0600

    add viewholder

--- app/src/main/java/com/example/mypizza/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mypizza/RecyclerViewAdapter.java
@@ -1,4 +1,50 @@
 package com.example.mypizza;
 
-public class RecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView imageView;
+        public TextView tvNewsTitle;
+        public TextView tvNewsSubtitle;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.news_image_view);
+            tvNewsTitle = itemView.findViewById(R.id.tv_news_title);
+            tvNewsSubtitle = itemView.findViewById(R.id.tv_news_subtitle);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate
+                (R.layout.home_recycler_view_items,
+                        parent,
+                        false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
