3d9cf75692c44bd41bdee260b2d4377b79b8e210
admin <kkanat94@gmail.com>
2021-02-22 00:11:33 +0600

    add new class

--- app/src/main/java/com/example/mypizza/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/mypizza/Fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.mypizza.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +22,9 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     private ArrayList<RecyclerViewItem> recyclerViewItems;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
---
+++ app/src/main/java/com/example/mypizza/RecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package com.example.mypizza;
+
+public class RecyclerViewAdapter {
+}
